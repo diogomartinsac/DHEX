ccb71d131ac154a6b60967d522b8cec394b01aff
Diogo Martins <diogomartins.ac@gmail.com>
2020-11-03 20:03:46 -0300

    get wheel_velocity from /dhex/odom

--- dhex_localization/src/tachometer_node.cpp
+++ dhex_localization/src/tachometer_node.cpp
@@ -1,6 +1,7 @@
 #include<ros/ros.h>
 #include<dhex_localization/Tachometer.h>
 #include <std_msgs/Float64.h>
+#include <nav_msgs/Odometry.h>
 
 class Parser 
 {
@@ -12,20 +13,41 @@ public:
     Parser(ros::NodeHandle nh);
 };
 
+double vr; 
+double va; 
+
+void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
+{
+  vr = msg ->twist.twist.linear.x;
+  va = msg ->twist.twist.angular.z;
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "tachometer");    
     ros::NodeHandle nh;
-    ros::Rate loop_rate(100);    
+    ros::Rate loop_rate(250);    
     Parser parser(nh);
     ros::Publisher velocity_publisher;
     ros::Publisher period_publisher;
     velocity_publisher = nh.advertise<std_msgs::Float64>("/wheel_velocity",10);
     period_publisher = nh.advertise<std_msgs::Float64>("/measurement_period",10);
+    
+    ros::Subscriber odom = nh.subscribe("/dhex/odom", 1, odomCallback);
+    
+    
     Tachometer tachometer(parser.wheel_name, parser.base_name,
                           parser.wheel_radius, parser.right_wheel);
     while(ros::ok())
     { 
+        // double vr; 
+        // double va; 
+
+        // calculate the left and right wheel speeds in rads/s
+        double wheel_speed_left = (vr - va * 0.09 / 2.0) / parser.wheel_radius;
+        double wheel_speed_right = (vr + va * 0.09 / 2.0) / parser.wheel_radius;
+
+        std::cout<<"Right Wheel speed: "<< wheel_speed_right<<" Left wheel speed: " << wheel_speed_left<<std::endl;
         tachometer.computeWheelVelocity();  
         if (!std::isnan(tachometer.getWheelVelocity().data)) {
             velocity_publisher.publish(tachometer.getWheelVelocity());
