12e3eb7e9fa39a1d85e3f1fe1191f5aa5f751a37
Diogo Martins <diogomartins.ac@gmail.com>
2020-11-03 20:33:34 -0300

    Get wheel_velocity from lookupTwist

--- dhex_localization/include/dhex_localization/Tachometer.h
+++ dhex_localization/include/dhex_localization/Tachometer.h
@@ -2,24 +2,20 @@
 #define TACHOMETER_H
 
 #include <ros/ros.h>
-#include <tf2_ros/transform_listener.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <tf/transform_listener.h>
 #include <std_msgs/Float64.h>
 
 class Tachometer 
 {
 private:
-    bool m_first_measure_flag;
     bool m_right_wheel;
-    double m_last_angle;
     std::string m_wheel_name;
     std::string m_base_name;
     double m_radius;
     std_msgs::Float64 m_velocity; 
-    tf2_ros::Buffer m_tf_buffer;
-    tf2_ros::TransformListener m_tf_listener;
-    ros::Time m_last_loop_time;
-    ros::Duration m_dt;
+    geometry_msgs::Twist m_tf_velocity;
+    tf::TransformListener m_tf_listener;
 
 public:
     Tachometer(const std::string &wheel_name, const std::string &base_name,
@@ -30,10 +26,6 @@ public:
     void computeWheelVelocity();
 
     std_msgs::Float64 getWheelVelocity();
-
-    std_msgs::Float64 getPeriod();
-
-    static double normilizeAngle(const double angle);
 };
 
 #endif
--- dhex_localization/src/tachometer_node.cpp
+++ dhex_localization/src/tachometer_node.cpp
@@ -2,6 +2,7 @@
 #include<dhex_localization/Tachometer.h>
 #include <std_msgs/Float64.h>
 #include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Twist.h>
 
 class Parser 
 {
@@ -10,58 +11,46 @@ public:
     std::string base_name, wheel_name;
     bool right_wheel;
 
-    Parser(ros::NodeHandle nh);
+    Parser(ros::NodeHandle &nh);
+    void deleteParameters(ros::NodeHandle &nh);
 };
-
-double vr; 
-double va; 
-
-void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
-{
-  vr = msg ->twist.twist.linear.x;
-  va = msg ->twist.twist.angular.z;
-}
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "tachometer");    
-    ros::NodeHandle nh;
-    ros::Rate loop_rate(250);    
+    ros::NodeHandle nh;    
     Parser parser(nh);
     ros::Publisher velocity_publisher;
-    ros::Publisher period_publisher;
-    velocity_publisher = nh.advertise<std_msgs::Float64>("/wheel_velocity",10);
-    period_publisher = nh.advertise<std_msgs::Float64>("/measurement_period",10);
-    
-    ros::Subscriber odom = nh.subscribe("/dhex/odom", 1, odomCallback);
-    
-    
     Tachometer tachometer(parser.wheel_name, parser.base_name,
                           parser.wheel_radius, parser.right_wheel);
+    velocity_publisher = 
+        nh.advertise<std_msgs::Float64>("localization/tachometer/" + parser.wheel_name +"_velocity",10);
+    parser.deleteParameters(nh);
+    ros::Rate loop_rate(10);    
+
     while(ros::ok())
     { 
-        // double vr; 
-        // double va; 
-
-        // calculate the left and right wheel speeds in rads/s
-        double wheel_speed_left = (vr - va * 0.09 / 2.0) / parser.wheel_radius;
-        double wheel_speed_right = (vr + va * 0.09 / 2.0) / parser.wheel_radius;
-
-        std::cout<<"Right Wheel speed: "<< wheel_speed_right<<" Left wheel speed: " << wheel_speed_left<<std::endl;
         tachometer.computeWheelVelocity();  
         if (!std::isnan(tachometer.getWheelVelocity().data)) {
             velocity_publisher.publish(tachometer.getWheelVelocity());
-            period_publisher.publish(tachometer.getPeriod());
         }
         ros::spinOnce(); 
         loop_rate.sleep();
     }
 }
 
-Parser::Parser(ros::NodeHandle nh)
+Parser::Parser(ros::NodeHandle &nh)
 {
     nh.getParam("wheel_radius", this->wheel_radius);
     nh.getParam("base_name", this->base_name);
     nh.getParam("wheel_name", this->wheel_name);
     nh.getParam("right_wheel", this->right_wheel);
+}
+
+void Parser::deleteParameters(ros::NodeHandle &nh)
+{    
+    nh.deleteParam("wheel_radius");
+    nh.deleteParam("base_name");
+    nh.deleteParam("wheel_name");
+    nh.deleteParam("right_wheel");
 }
